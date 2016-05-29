de3f58f7dd8a9989ffa034a78d5f8fd22ac5da07
Fernando Ulloa <fjulloap23@gmail.com>
2016-05-29 10:12:49 -0600

    Add bytevec docs

---
+++ doc/implementors/bytevec/trait.ByteEncodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bytevec'] = [];implementors["bytevec"] = [];implementors["bytevec"] = [];implementors['bytevec'] = [];implementors['bytevec'] = [];implementors['bytevec'] = [];implementors['bytevec'] = [];implementors['bytevec'] = [];implementors['bytevec'] = [];implementors['bytevec'] = [];implementors['bytevec'] = [];implementors['bytevec'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
